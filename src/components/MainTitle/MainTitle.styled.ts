a92fe3b9413d8f305c55eac231660cfd82b68984
Lisnyi <russelhobbbs@gmail.com>
2023-08-02 12:28:08 +0300

    add main title

---
+++ src/components/MainTitle/MainTitle.styled.ts
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+export const Title = styled.a`
+    font-family: 'AlataRegular', sans-serif;
+    font-size: ${p => p.theme.fontSizes.semiLarge};
+    color: ${p => p.theme.colors.blue};
+    cursor: pointer;
+`
---
+++ src/components/MainTitle/MainTitle.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import { Title } from "./MainTitle.styled"
+
+type Props = {
+    title: string
+}
+
+export const MainTitle: FC<Props> = ({title}) => {
+  return (
+    <Title>
+        {title}
+    </Title>
+  )
+}
