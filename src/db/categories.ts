1dac6b7fb4a869528e2aeb037b0f2177dc1c0e91
Lisnyi <russelhobbbs@gmail.com>
2023-08-07 23:39:47 +0300

    add categories and priority base

---
+++ src/db/categories.ts
@@ -0,0 +1,25 @@
+import type { CategoryObj } from "../types"
+
+export const categories: Array<CategoryObj> = [
+	{
+		value: 'Art',
+	},
+	{
+		value: 'Music',
+	},
+	{
+		value: 'Business',
+	},
+	{
+		value: 'Conference',
+	},
+	{
+		value: 'Workshop',
+	},
+	{
+		value: 'Party',
+	},
+	{
+		value: 'Sport',
+	},
+]
---
+++ src/db/priority.ts
@@ -0,0 +1,13 @@
+import type { PriorityObj } from "../types"
+
+export const priority: Array<PriorityObj> = [
+	{
+		value: 'High',
+	},
+	{
+		value: 'Medium',
+	},
+	{
+		value: 'Low',
+	}
+]
