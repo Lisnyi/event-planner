e53f58e0bfb4fca095b4145b49b70aede284fabf
Lisnyi <russelhobbbs@gmail.com>
2023-08-07 23:38:16 +0300

    add FormSelect

---
+++ src/components/forms/FormikControl/Select/Select.styled.ts
@@ -0,0 +1,62 @@
+import styled, { DefaultTheme } from 'styled-components';
+import { makeStyles } from 'tss-react/mui'
+import { Select } from '@mui/base'
+import ArrowDown from '../../../../assets/icons/chevron-down.svg'
+import ArrowUp from '../../../../assets/icons/chevron-up.svg'
+
+export const SelectButton = styled(Select)`
+    &.MuiSelect-root {
+        text-align: left;
+        height: 56px;
+        padding: 16px 12px;
+        border: 1px solid ${p => p.theme.colors.grey};
+        border-radius: 8px;
+        background-color: white;
+        color: ${p => p.theme.colors.blue};
+        font-family: 'PoppinsRegular', sans-serif;
+        font-size: 16px;
+        line-height: 24px;
+        cursor: pointer;
+
+        &:hover, &:focus {
+            border-color: ${p => p.theme.colors.accentBlue};
+        }
+
+        &::after {
+            content: ${p => p.listboxOpen ? `url(${ArrowUp})` : `url(${ArrowDown})`};
+            width: 24px;
+            height: 24px;
+            float: right;
+        }
+    }
+`
+
+export const useStyles = makeStyles<{ theme: DefaultTheme }>()(
+    (_, { theme }) => ({
+        "popper": {
+            width: '240px',
+            padding: '0 16px',
+            borderRadius: '8px',
+            backgroundColor: 'white',
+            boxShadow: theme.shadow.main,
+        },
+        "list": {
+            margin: 0,
+            padding: 0,
+            listStyle: 'none',
+            backgroundColor: 'white',
+
+            "& li:not(:last-child)": {
+                borderBottom: `1px solid ${theme.colors.grey}`,
+            },
+            "& li": {
+                padding: '16px 0',
+                fontSize: theme.fontSizes.medium,
+                fontFamily: "PoppinsRegular, sans-serif",
+                color: theme.colors.black,
+            },
+            "& li.Mui-selected": {
+                color: theme.colors.accentBlue,
+            },
+        },
+}))
---
+++ src/components/forms/FormikControl/Select/Select.tsx
@@ -0,0 +1,46 @@
+import { FC, useState } from 'react'
+import { useFormikContext, useField } from 'formik'
+import { useTheme } from 'styled-components'
+import { Option } from '@mui/base'
+import { FormControlBox, Label, ErrorMessage } from '..'
+import { SelectButton, useStyles } from './Select.styled'
+import type { CreateEventFormValues } from '../../../../types'
+
+type Props = {
+	id: string,
+	name: string,
+	label: string,
+	options: Array<{value: string}> | undefined
+}
+
+export const Select: FC<Props> = ({id, label, name, options, ...rest}) => {
+
+	const { values, errors, touched, handleChange, handleBlur } = useFormikContext<CreateEventFormValues>();
+	const [, , {setValue}] = useField(name);
+	const theme = useTheme()
+	const { classes } = useStyles({"theme": theme});
+	const errorMessage = touched[name as keyof CreateEventFormValues] && errors[name as keyof CreateEventFormValues]
+	const error = touched[name as keyof CreateEventFormValues] && Boolean(errors[name as keyof CreateEventFormValues])
+	const [isListOpen, setIsListOpen] = useState(false)
+	
+	return (
+		<FormControlBox>
+			<Label htmlFor={id} label={label}/>
+			<SelectButton
+				id={id}
+				name={name}
+				value={values[name as keyof CreateEventFormValues]}
+				onChange={handleChange}
+				onBlur={handleBlur}
+				error={error}
+				listboxOpen={isListOpen}
+				onListboxOpenChange={() => setIsListOpen(!isListOpen)}
+				slotProps={{listbox: { className: classes.list }, popper: { className: classes.popper }}}
+				{...rest}>
+				<Option value="">Select a {name}</Option>
+				{options && options.map(({value}, index) => <Option onClick={() => setValue(value, true)} key={index} value={value}>{value}</Option>)}
+			</SelectButton>
+			{errorMessage && <ErrorMessage message={errorMessage}/>}
+		</FormControlBox>
+	)
+}
