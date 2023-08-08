fc6299dc68236374b9353b1f944ac8ee21798c1a
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 16:44:51 +0300

    Update NotFoundPage.tsx

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { FC } from 'react'
+import { Container, Section, GoBackButton, PageTitle } from '../components'
 
-const NotFoundPage = () => {
-  return (
-    <div>NotFoundPage</div>
-  )
+const NotFoundPage: FC = () => {
+	return (
+		<Container>
+			<Section>
+				<GoBackButton/>
+				<PageTitle content='Page Not Found'/>
+			</Section>
+		</Container>
+	)
 }
 
 export default NotFoundPage
