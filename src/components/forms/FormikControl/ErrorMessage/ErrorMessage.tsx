7393bb019c74904bcece54b29dd8f5a37f85a7d5
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 17:30:51 +0300

    Update ErrorMessage.tsx

--- src/components/forms/FormikControl/ErrorMessage/ErrorMessage.tsx
+++ src/components/forms/FormikControl/ErrorMessage/ErrorMessage.tsx
@@ -1,14 +1,18 @@
 import { FC } from 'react'
+import { FormikErrors } from 'formik'
 import { Message } from './ErrorMessage.styled'
+import { Dayjs } from 'dayjs'
 
 type Props = {
-    message: string,
+    message: string | FormikErrors<Dayjs>,
 }
 
 export const ErrorMessage: FC<Props> = ({message}) => {
-    return (
-        <Message>
-            {message}
-        </Message>
-    )
+    if (typeof message === 'string') {
+        return (
+            <Message>
+                {message}
+            </Message>
+        )
+    }
 }
