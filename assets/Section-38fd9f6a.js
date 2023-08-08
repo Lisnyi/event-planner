2582dd857393429efe6e5f2d997260860a58108f
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 18:26:57 +0300

    Adding dist

---
+++ assets/Section-38fd9f6a.js
@@ -0,0 +1,1 @@
+import{j as s,bn as t,bo as n}from"./index-6b64c674.js";const x=({children:o})=>s.jsx(t,{children:o}),a=({children:o})=>s.jsx(n,{children:o});export{x as C,a as S};
--- assets/Section-c39df09b.js
+++
@@ -1,1 +0,0 @@
-import{j as s,bn as t,bo as n}from"./index-49aaa6b4.js";const x=({children:o})=>s.jsx(t,{children:o}),a=({children:o})=>s.jsx(n,{children:o});export{x as C,a as S};
