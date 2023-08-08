f99c3bab017a7ffaa52ee7ec746ff51e5f9e2cb9
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 23:03:36 +0300

    update base url

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
             <ThemeProvider theme={theme}>
-                <BrowserRouter basename="/">
+                <BrowserRouter basename="/event-planner">
                     <GlobalStyle/>
                     <Suspense fallback={<Loader/>}>
                         <App />
