3cbeef9fff5ada618f41b14338695deffb1ba4f8
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 16:45:01 +0300

    Update EventInfoPage.tsx

--- src/pages/EventInfoPage.tsx
+++ src/pages/EventInfoPage.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { FC } from 'react'
+import { useParams } from 'react-router-dom'
+import { Container, Section, GoBackButton, PageTitle, EventInfoCard } from '../components'
+import { useGetEventByIdQuery } from '../redux'
 
-const EventInfoPage = () => {
-  return (
-    <div>EventInfoPage</div>
-  )
+const EventInfoPage: FC = () => {
+
+    const { eventId } = useParams<{eventId: string}>()
+    const { data, isError } = useGetEventByIdQuery(eventId)
+
+    return (
+        <Container>
+            <Section>
+                <GoBackButton/>
+                {data && <>
+                            <PageTitle content={data.title}/>
+                            <EventInfoCard event={data}/>
+                        </>}
+                {isError && <PageTitle content='Oops, something went wrong'/>}
+            </Section>
+        </Container>
+    )
 }
 
 export default EventInfoPage
