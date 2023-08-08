678166d19dc216500f1c88c1ea07cb560e3245be
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 17:30:47 +0300

    Update CreateEventForm.styled.ts

--- src/components/forms/CreateEventForm/CreateEventForm.styled.ts
+++ src/components/forms/CreateEventForm/CreateEventForm.styled.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { Form } from "formik";
 
 export const CustomForm = styled(Form)`
-    width: 272px;
     margin: 0 auto;
     padding: 40px 16px;
     background-color: white;
