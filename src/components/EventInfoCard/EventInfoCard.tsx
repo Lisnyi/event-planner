109a6b0e94bd9b06efab7293ae8c2cdababae9ce
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 16:41:00 +0300

    add logic to EventInfoCard

--- src/components/EventInfoCard/EventInfoCard.tsx
+++ src/components/EventInfoCard/EventInfoCard.tsx
@@ -1,35 +1,58 @@
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
+import * as dayjs from 'dayjs'
 import { EventInfoButton, Tag, PriorityTag } from '..'
 import { Card, CardImageBox, CardImage, CardDescriptionBox, CardDescription, TagsBox, ButtonsBox } from './EventInfoCard.styled'
-import Foto from '../../assets/images/Rectangle 330.png'
+import { WideDefaultImage } from '../../assets'
+import { useDeleteEventMutation } from '../../redux'
+import type { Event } from '../../types'
 
-export const EventInfoCard: FC = () => {
+type Props = {
+    event: Event
+}
 
+export const EventInfoCard: FC<Props> = ({event}) => {
+
+    const { id, picture, description, category, priority, location, date, time } = event
+    
     const navigate = useNavigate()
+    const [ deleteEvent ] = useDeleteEventMutation()
+    
+    const dateFormat = dayjs(date).format("DD.MM")
+    const timeFormat =  dayjs(time).format("HH:mm")
 
     function goToEditEventPage() {
-        navigate("/edit-event")
+        navigate(`/edit-event/${id}`)
+    }
+    function handleDelete() {
+        try{
+            deleteEvent(id)
+            .unwrap()
+            .then(()=> navigate(`/`))
+        }
+        catch(e){
+            console.log(e)
+        }
     }
 
     return (
         <Card>
             <CardImageBox>
-                <CardImage src={Foto} alt='Event image'/>
+                <CardImage src={picture ? picture : WideDefaultImage} alt='Event image'/>
             </CardImageBox>
             <CardDescriptionBox>
                 <CardDescription>
-                    Discover an enchanting evening celebrating the world of art at our exclusive gallery opening.
+                    {description}
                 </CardDescription>
                 <TagsBox>
-                    <Tag content='Art'/>
-                    <PriorityTag content='High'/>
-                    <Tag content='Kyiv'/>
-                    <Tag content='12.07 at 12:00'/>
+                    <Tag content={category}/>
+                    <PriorityTag content={priority}/>
+                    <Tag content={location}/>
+                    <Tag content={`${dateFormat} at ${timeFormat}`}/>
                 </TagsBox>
                 <ButtonsBox>
                     <EventInfoButton secondary content='Edit' handleClick={goToEditEventPage}/>
-                    <EventInfoButton content='Delete event' handleClick={()=>console.log('click')}/>
+                    <EventInfoButton content='Delete event' handleClick={handleDelete}/>
                 </ButtonsBox>
             </CardDescriptionBox>
         </Card>
