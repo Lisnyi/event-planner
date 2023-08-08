a421ea71c7460e6fc9c57b5d1a750da7d9b3eb91
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 16:39:00 +0300

    Create index.ts

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import BackGround from './bg.jpg'
+import DefaultImage from './default.png'
+import WideDefaultImage from './default-wide.png'
+
+export {
+    BackGround,
+    DefaultImage,
+    WideDefaultImage
+}
