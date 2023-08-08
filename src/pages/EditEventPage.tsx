b7c1869d948949197ea5f42c43a1d952d49bbc24
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 17:31:41 +0300

    Update EditEventPage.tsx

--- src/pages/EditEventPage.tsx
+++ src/pages/EditEventPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { FC } from 'react'
+import { useParams } from 'react-router-dom'
+import { Container, Section, GoBackButton, PageTitle, UpdateEventForm } from '../components'
+import { useGetEventByIdQuery } from '../redux'
 
-const EditEventPage = () => {
-  return (
-    <div>EditEventPage</div>
-  )
+const EditEventPage: FC = () => {
+
+    const { eventId } = useParams<{eventId: string}>()
+    const { data } = useGetEventByIdQuery(eventId)
+
+    return (
+        <Container>
+            <Section>
+                <GoBackButton/>
+                <PageTitle content='Edit event'/>
+                {data && <UpdateEventForm event={data}/>}
+            </Section>
+        </Container>
+    )
 }
 
 export default EditEventPage
