10caed7896c2fc5e87b38ca76eb0a13ea62443d8
Lisnyi <russelhobbbs@gmail.com>
2023-08-08 17:32:12 +0300

    add UpdateEventFormValuesType

--- src/types/eventFormValues.ts
+++ src/types/eventFormValues.ts
@@ -10,3 +10,14 @@ export type CreateEventFormValues = {
     picture: string,
     priority: string,
 }
+
+export type UpdateEventFormValues = {
+    title: string,
+    description: string,
+    date: Dayjs,
+    time: Dayjs,
+    location: string,
+    category: string,
+    picture: string,
+    priority: string,
+}
